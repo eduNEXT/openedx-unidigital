63e48c85b8dc82c23451f8443e182ced1f0d0a2f
Cristhian Garcia <crisgarta8@gmail.com>
2023-11-22 14:44:00 -0500

    chore: initial commit

---
+++ openedx_unidigital/__init__.py
@@ -0,0 +1,5 @@
+"""
+An Open edX plugin for the Unidigital project
+"""
+
+__version__ = '0.1.0'
---
+++ openedx_unidigital/apps.py
@@ -0,0 +1,13 @@
+"""
+openedx_unidigital Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class OpenedxUnidigitalConfig(AppConfig):
+    """
+    Configuration for the openedx_unidigital Django application.
+    """
+
+    name = 'openedx_unidigital'
