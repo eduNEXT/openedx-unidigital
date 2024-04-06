a24e76ed2b9706684994b682ac73008e39e0ea9e
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-04-06 18:28:08 -0500

    chore: add django_crum in base requirements

--- openedx_unidigital/handlers.py
+++ openedx_unidigital/handlers.py
@@ -1,23 +1,20 @@
 """Event handlers for the Open edX Unidigital plugin."""
 
-from xmodule.modulestore.django import modulestore
-from django.conf import settings
 from crum import get_current_request
+from django.conf import settings
 
-from openedx.core.djangoapps.user_api.preferences.api import get_user_preference
+from openedx_unidigital.edxapp_wrapper.course_groups import add_user_to_cohort
+from openedx_unidigital.edxapp_wrapper.lang_pref import LANGUAGE_KEY
+from openedx_unidigital.edxapp_wrapper.modulestore import modulestore
+from openedx_unidigital.edxapp_wrapper.teams import get_team_by_team_id
+from openedx_unidigital.edxapp_wrapper.user_preferences import get_user_preference
 
-from openedx.core.djangoapps.lang_pref import LANGUAGE_KEY
 
-
-def add_member_to_team_by_language(signal, sender, enrollment, metadata, **kwargs):
+def add_member_to_team_by_language(
+    signal, sender, enrollment, metadata, **kwargs
+) -> None:
     """
-    Membership by language configuration.
-
-    Args:
-        signal (_type_): _description_
-        sender (_type_): _description_
-        enrollment (_type_): _description_
-        metadata (_type_): _description_
+    Add user to team/cohort by language.
     """
     course_key = enrollment.course.course_key
     course_block = modulestore().get_course(course_key)
@@ -26,4 +23,26 @@ def add_member_to_team_by_language(signal, sender, enrollment, metadata, **kwarg
     )
     request = get_current_request()
     user = request.user
-    language_preference = get_user_preference(user, LANGUAGE_KEY)
+    language_preference = (
+        get_user_preference(user, LANGUAGE_KEY) or settings.LANGUAGE_CODE
+    )
+
+    if language_preference in membership_by_language:
+        course_groups = membership_by_language[language_preference]
+        for group in course_groups:
+            if group["type"] == "team":
+                add_user_to_team(user, group["id"])
+            elif group["type"] == "cohort":
+                add_user_to_cohort(user, group["id"])
+
+
+def add_user_to_team(user, team_id: str) -> None:
+    """
+    Add user to team.
+
+    Args:
+        user (User): The user object.
+        team_id (str): The team id.
+    """
+    team = get_team_by_team_id(team_id)
+    team.add_user(user)
