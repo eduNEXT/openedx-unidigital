7a1a9d02b2b13de9a0a62ec4a40ec547e6752e04
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-04-07 08:26:28 -0500

    chore: fix quality checks

--- openedx_unidigital/edxapp_wrapper/backends/course_groups_q_v1.py
+++ openedx_unidigital/edxapp_wrapper/backends/course_groups_q_v1.py
@@ -2,4 +2,6 @@
 Course Groups definitions for Open edX Quince release.
 """
 
-from openedx.core.djangoapps.course_groups.cohorts import add_user_to_cohort
+from openedx.core.djangoapps.course_groups.cohorts import (  # pylint: disable=import-error, unused-import
+    add_user_to_cohort,
+)
--- openedx_unidigital/edxapp_wrapper/backends/user_preferences_q_v1.py
+++ openedx_unidigital/edxapp_wrapper/backends/user_preferences_q_v1.py
@@ -2,4 +2,6 @@
 User Preferences definitions for Open edX Quince release.
 """
 
-from openedx.core.djangoapps.user_api.preferences.api import get_user_preference  # pylint: disable=import-error, unused-import
+from openedx.core.djangoapps.user_api.preferences.api import (  # pylint: disable=import-error, unused-import
+    get_user_preference,
+)
