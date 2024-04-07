256316c50d0a69b71ab5d0e2b328222553db3d61
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-04-07 15:18:46 -0500

    feat: add error classess in backends

--- openedx_unidigital/edxapp_wrapper/backends/teams_q_v1.py
+++ openedx_unidigital/edxapp_wrapper/backends/teams_q_v1.py
@@ -3,3 +3,8 @@ Teams definitions for Open edX Quince release.
 """
 
 from lms.djangoapps.teams.api import get_team_by_team_id  # pylint: disable=import-error, unused-import
+from lms.djangoapps.teams.errors import (  # pylint: disable=import-error, unused-import
+    AddToIncompatibleTeamError,
+    AlreadyOnTeamInTeamset,
+    NotEnrolledInCourseForTeam,
+)
--- openedx_unidigital/edxapp_wrapper/teams.py
+++ openedx_unidigital/edxapp_wrapper/teams.py
@@ -15,3 +15,38 @@ def get_team_by_team_id(*args, **kwargs):
     backend = import_module(backend_function)
 
     return backend.get_team_by_team_id(*args, **kwargs)
+
+
+def get_already_on_team_in_teamset_error():
+    """
+    Wrapper for `teams.errors.AlreadyOnTeamInTeamset`
+    """
+    backend_function = settings.OPENEDX_UNIDIGITAL_TEAMS_BACKEND
+    backend = import_module(backend_function)
+
+    return backend.AlreadyOnTeamInTeamset
+
+
+def get_not_enrolled_in_course_for_team_error():
+    """
+    Wrapper for `teams.errors.NotEnrolledInCourseForTeam`
+    """
+    backend_function = settings.OPENEDX_UNIDIGITAL_TEAMS_BACKEND
+    backend = import_module(backend_function)
+
+    return backend.NotEnrolledInCourseForTeam
+
+
+def get_add_to_incompatible_team_error():
+    """
+    Wrapper for `teams.errors.AddToIncompatibleTeamError`
+    """
+    backend_function = settings.OPENEDX_UNIDIGITAL_TEAMS_BACKEND
+    backend = import_module(backend_function)
+
+    return backend.AddToIncompatibleTeamError
+
+
+AddToIncompatibleTeamError = get_add_to_incompatible_team_error()
+AlreadyOnTeamInTeamset = get_already_on_team_in_teamset_error()
+NotEnrolledInCourseForTeam = get_not_enrolled_in_course_for_team_error()
