f4fed8e6618ea88cfa1a99442417a8adce95947e
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-04-10 13:17:39 -0400

    feat: add implementation for denying access

--- openedx_unidigital/roles.py
+++ openedx_unidigital/roles.py
@@ -1,12 +1,13 @@
 from common.djangoapps.student.models import CourseAccessRole
-from common.djangoapps.student.roles import CourseStaffRole
+from common.djangoapps.student.roles import CourseStaffRole, RoleCache
+from crum import get_current_request, get_current_user
+from pprint import pprint
 
 class CourseStaffRoleMonkeyPatched(CourseStaffRole):
     """
     Roles by type (e.g., instructor, beta_user) and optionally org, course_key
     """
 
-    # pylint: disable=arguments-differ
     def has_user(self, user, check_user_activation=True):
         """
         Check if the supplied django user has access to this role.
@@ -18,17 +19,40 @@ class CourseStaffRoleMonkeyPatched(CourseStaffRole):
         Return:
             bool identifying if user has that particular role or not
         """
-        print("HAS USER MONKEY PATCHED -------------------------------------")
-        import pudb; pudb.set_trace()
         if check_user_activation and not (user.is_authenticated and user.is_active):
             return False
 
-        if CourseAccessRole.objects.filter(user=user, role='limited_staff').exists():
-            print("USER IS LIMITED STAFF -------------------------------------")
-        # # pylint: disable=protected-access
-        # if not hasattr(user, '_roles'):
-        #     # Cache a list of tuples identifying the particular roles that a user has
-        #     # Stored as tuples, rather than django models, to make it cheaper to construct objects for comparison
-        #     user._roles = RoleCache(user)
+        print("JUGANDO2 CON PERMISOS en RoleBase.has_user")
 
-        return user._roles.has_role(self._role_name, self.course_key, self.org)
+        original_return_value = user._roles.has_role(self._role_name, self.course_key, self.org)
+
+        crum_request = get_current_request()
+        crum_user = get_current_user()
+        post_student = crum_request.POST.get("unique_student_identifier", crum_request.POST.get("student", None))
+
+        if all([
+                "/instructor/api/" in crum_request.path,
+                original_return_value == True,
+                crum_request.method == 'POST',
+                self._role_name == "staff", # como podriamos validar que es 'limited staff' y no solo 'staff'
+                post_student,
+            ]):
+
+            print("JUGANDO CON PERMISOS en RoleBase.has_user")
+            print(original_return_value)
+            pprint(post_student)
+            pprint(self._role_name)
+            pprint(crum_request.POST)
+            pprint(self)
+            # pprint(dir(self))
+
+            if post_student == "felipe":  # voy a bloquear que limited no pueda alterar a 'felipe'
+                return False
+
+        # pylint: disable=protected-access
+        if not hasattr(user, '_roles'):
+            # Cache a list of tuples identifying the particular roles that a user has
+            # Stored as tuples, rather than django models, to make it cheaper to construct objects for comparison
+            user._roles = RoleCache(user)
+
+        return original_return_value
