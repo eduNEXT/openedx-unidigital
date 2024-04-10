25876b25f8206a43d53c053d00cb51e3b85c973d
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-04-10 15:03:05 -0400

    feat: add custom bridgekeeper rules backend

--- openedx_unidigital/settings/common.py
+++ openedx_unidigital/settings/common.py
@@ -7,3 +7,9 @@ def plugin_settings(settings):
     """
     Read / Update necessary common project settings.
     """
+    if settings.SERVICE_VARIANT == 'lms':
+        settings.AUTHENTICATION_BACKENDS = [
+            'rules.permissions.ObjectPermissionBackend',
+            'django.contrib.auth.backends.AllowAllUsersModelBackend',
+            'openedx_unidigital.backends.UnidigitalRulesBackend'
+        ]
