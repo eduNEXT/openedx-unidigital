bcf702e33f6c9b782f8dea28f54d07c1c9a76373
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-04-10 15:12:59 -0400

    refactor: inherit from class instead of creating new one

--- openedx_unidigital/settings/common.py
+++ openedx_unidigital/settings/common.py
@@ -8,8 +8,5 @@ def plugin_settings(settings):
     Read / Update necessary common project settings.
     """
     if settings.SERVICE_VARIANT == 'lms':
-        settings.AUTHENTICATION_BACKENDS = [
-            'rules.permissions.ObjectPermissionBackend',
-            'django.contrib.auth.backends.AllowAllUsersModelBackend',
-            'openedx_unidigital.backends.UnidigitalRulesBackend'
-        ]
+        settings.AUTHENTICATION_BACKENDS.append('openedx_unidigital.roles.UnidigitalRulesBackend')
+        settings.AUTHENTICATION_BACKENDS.remove('bridgekeeper.backends.RulePermissionBackend')
