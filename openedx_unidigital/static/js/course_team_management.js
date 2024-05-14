54c3550b8b2b60e11538696489a5d24aaa04f525
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-05-14 12:10:54 -0400

    fix: remove empty django.mo files

--- openedx_unidigital/static/js/course_team_management.js
+++ openedx_unidigital/static/js/course_team_management.js
@@ -116,7 +116,7 @@ $("#course-teamset").on("change", function (select) {
   });
 
 $(function () {
-  const cssUrl = "https://cdn.jsdelivr.net/npm/@openedx/paragon@22.4.0/dist/paragon.min.css";
+  const cssUrl = "https://cdn.jsdelivr.net/npm/@edx/paragon@20.45.5/dist/paragon.min.css";
 
   $("<link>")
     .attr({
